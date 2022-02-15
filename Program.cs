bec015fdfedb55160f7202e1c47ac160d6a699b3
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2022-02-16 01:19:41 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetCrashTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
